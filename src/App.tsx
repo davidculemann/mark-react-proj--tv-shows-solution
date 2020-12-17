2f8c49baa3c49830436c2120ef3cb993d4f97b21
nbogie <neillbogie@googlemail.com>
2020-12-17 14:39:35 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BabyNamesApp from "./BabyNames/BabyNamesApp";
+
+function App() {
+  return (
+    <div className="App">
+      <BabyNamesApp />
+    </div>
+  );
+}
+
+export default App;
