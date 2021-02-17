62ce20f122fdbbeb34a8726ece44919540bd1424
Esme Hotston Moore <ehotstonmoore@hotmail.co.uk>
2021-02-17 16:31:54 +0000

    add search functionality

--- src/TvShows/TvShowsApp.tsx
+++ src/TvShows/TvShowsApp.tsx
@@ -2,17 +2,17 @@
 //Flow diagram: https://excalidraw.com/#json=5680880538353664,5FOVmiVqJ_XfHphPRCxGCA
 
 import React from "react";
-import EpisodesData from "./EpisodesData.json";
+import AllEpisodesData from "./EpisodesData.json";
 import Header from "./Header";
 import Footer from "./Footer";
-import EpisodeList from "./EpisodeList";
+import SearchableEpisodeList from "./SearchableEpisodeList";
 import "./TvShowsApp.css";
 
 function TvShowsApp() {
   return (
     <div className="App">
       <Header />
-      <EpisodeList episodes={EpisodesData} />
+      <SearchableEpisodeList episodes={AllEpisodesData} />
       <Footer />
     </div>
   );
