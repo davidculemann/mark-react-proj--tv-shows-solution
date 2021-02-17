3c76c6e6d25807a1fdcd118b778ea79d502771f2
Esme Hotston Moore <ehotstonmoore@hotmail.co.uk>
2021-02-17 16:52:49 +0000

    strip tags from episode summaries

--- src/TvShows/Episode.tsx
+++ src/TvShows/Episode.tsx
@@ -8,12 +8,29 @@ function EpisodeCard(props: EpisodeCardProps) {
         <div className="episode-card">
             <h1>{props.episode.name} - {makeEpisodeCode(props.episode)}</h1>
             <img src={props.episode.image.medium} alt="screengrab of episode" />
-            <p>{props.episode.summary}</p>
+            <p>{stripTags(props.episode.summary)}</p>
         </div>
     );
 }
 //The above assumes all episodes have images (not true for ALL shows).
 
-
+//Remove tags by replacing any matching occurrences with an empty string.
+//This function uses a regular expression.  
+//It is NOT required you understand regular expressions for the course.
+function stripTags(str: string) {
+    if (!str) {
+        return str;
+    }
+    //Components of the regex:
+    //
+    // <       a literal <
+    // \/ ?    a literal /  (zero or one occurrences)
+    // [a-z]+  a sequence of at least one alphabet character (see flags)
+    // >       a literal >
+    // gi      global and case-insensitive "flags"
+    //
+    //Further regex breakdown at https://regexr.com/532tm
+    return str.replace(/<\/?[a-z]+>/gi, "");
+}
 
 export default EpisodeCard;
