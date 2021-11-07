1c8f0b6753a470c194854604e3dbec4c6fe7e031
nbogie <neillbogie@googlemail.com>
2021-11-07 23:35:08 +0000

    correct level in header

--- src/TvShows/Header.tsx
+++ src/TvShows/Header.tsx
@@ -1,5 +1,5 @@
 function Header() {
-    return (<header>Level 101 Sample Solution - React and TypeScript</header>);
+    return (<header>Level 300 Sample Solution - React and TypeScript</header>);
 }
 
 export default Header;
